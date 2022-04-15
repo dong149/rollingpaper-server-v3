fd9d826e9d6c2ca6930adf07f595a9929d0c02e7
Patrick Ryoo <donghoon149@gmail.com>
2022-04-15 18:29:37 +0900

    intiial setting

---
+++ src/test/java/com/rollingpaper/api/RollingpaperServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rollingpaper.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollingpaperServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
